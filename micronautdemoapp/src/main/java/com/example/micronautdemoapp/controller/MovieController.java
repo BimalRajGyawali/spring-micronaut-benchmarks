2bb5c9f7e99a9ab2610ae985cbb76ae1aa00c9d1
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2023-04-04 23:48:53 +0545

    add log

--- micronautdemoapp/src/main/java/com/example/micronautdemoapp/controller/MovieController.java
+++ micronautdemoapp/src/main/java/com/example/micronautdemoapp/controller/MovieController.java
@@ -6,11 +6,15 @@ import com.example.micronautdemoapp.service.MovieService;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @Controller("/movies")
 public class MovieController {
+
+    private final static Logger logger = LoggerFactory.getLogger(MovieController.class);
 
     private final MovieService movieService;
 
@@ -20,6 +24,8 @@ public class MovieController {
 
     @Get
     public List<MovieData> getAllMoviesOfGenre(@QueryValue String genre){
+        logger.info("Request received {}", genre);
+
         return movieService.findAllMoviesOfGenre(genre).stream()
                 .map(movie -> new MovieData(movie.getTitle(), movie.getGenre(), movie.getImdbRating()))
                 .toList();
