2200e501fe3adcbf934afcefe5384e14b96fa105
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2023-04-04 23:48:26 +0545

    adds log

--- springdemoapp/src/main/java/com/example/springdemoapp/controller/MovieController.java
+++ springdemoapp/src/main/java/com/example/springdemoapp/controller/MovieController.java
@@ -2,6 +2,8 @@ package com.example.springdemoapp.controller;
 
 import com.example.springdemoapp.dto.MovieData;
 import com.example.springdemoapp.service.MovieService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +15,8 @@ import java.util.List;
 @RequestMapping("/movies")
 public class MovieController {
 
+    private final static Logger logger = LoggerFactory.getLogger(MovieController.class);
+
     private final MovieService movieService;
 
     public MovieController(MovieService movieService) {
@@ -21,6 +25,7 @@ public class MovieController {
 
     @GetMapping
     public List<MovieData> getAllMoviesOfGenre(@RequestParam String genre){
+        logger.info("Request received {}", genre);
         return movieService.findAllMoviesOfGenre(genre).stream()
                 .map(movie -> new MovieData(movie.getTitle(), movie.getGenre(), movie.getImdbRating()))
                 .toList();
